7e9a1a516540e6beb704f13b7269c3466f031080
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-06-09 21:26:37 -0700

    commit tree 5b16d577ecb08aa480aa7c4e2511d5bd9c3d0c94

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+
+  attr_accessor :kind, :a, :b, :c
+
+  def initialize (a, b, c)
+    @a = a
+    @b = b
+    @c = c 
+  end
+
+  def kind
+    if self.a > 0 and self.b > 0 and self.c > 0 and self.a + self.b > self.c and self.b + self.c > self.a and self.a + self.c > self.b
+      if self.a == self.b and self.b == self.c and self.a == self.c
+        self.kind = :equilateral
+      elsif self.a != self.b and self.b != self.c and self.a != self.c
+        self.kind = :scalene
+      else 
+        self.kind = :isosceles
+      end 
+    else 
+      begin
+        raise TriangleError
+      end
+    end   
+  end
+
+  class TriangleError < StandardError
+  end
 end
